dc433e2b5adfbb821e2bf14732785207fdb2e3ba
Mayu <61958475+consified@users.noreply.github.com>
2024-03-08 04:47:48 +0800

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ from dhooks import Webhook
 
 
 #prompts
-message = ("藍的店2號館的 \"maimai DX International Ver.\" 剛剛開機了!")
+message = ("Hello World!")
 url = (<your url>) #insert webhook url
 webhookurl = Webhook(url)
 webhookurl.send(message)
